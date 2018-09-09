5cded9e24a70a5bbcd4cc6f4226115392bf0f009
Howard Chen <chenh0886@gmail.com>
2018-09-09 02:27:07 -0700

    Change copy

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,7 @@ const Home = () => (
     <Nav />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next! ABOUT PAGE</h1>
+      <h1 className="title">ABOUT PAGE</h1>
       <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
