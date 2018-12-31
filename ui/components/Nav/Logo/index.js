030b8088fee9226c592af0fb6fbd30a21ac5e79e
Howard Chen <chenh0886@gmail.com>
2018-12-31 15:41:02 -0800

    Add hover state

--- ui/components/Nav/Logo/index.js
+++ ui/components/Nav/Logo/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { compose, withStateHandlers, withState } from 'recompose';
 import css from 'styled-jsx/css';
 import navSpacing from '../../../spacing/nav';
 import fontSize from '../../../spacing/fontSize';
@@ -8,20 +9,46 @@ const defaultStyle = css`
     left: 50%;
     max-width: ${navSpacing.logoMaxWidth};
     position: absolute;
+    text-align: center;
     transform: translateX(-50%);
+    z-index: 10;
   }
-  .logo-hover {
+  .logo-eyes {
     font-size: ${fontSize.xl2};
+    cursor: default;
   }
 `;
 
-export default () => (
-  <React.Fragment>
-    <div className="logo">
-      <img src="asd" />
-      <div className="logo-hover">👀</div>
-    </div>
-    
-    <style jsx>{defaultStyle}</style>
-  </React.Fragment>
-)
+export default compose(
+  withStateHandlers(
+    () => ({
+      hovered: false
+    }),
+    {
+      hoverOn: () => () => ({ hovered: true }),
+      hoverOff: () => () => ({ hovered: false })
+    }
+  )
+)(
+  ({ hovered, hoverOn, hoverOff }) => (
+    <React.Fragment>
+      <div className="logo" onMouseEnter={hoverOn} onMouseLeave={hoverOff}>
+        <img src="/static/logo-black.png"/>
+        <div className="logo-eyes">👀</div>
+      </div>
+      
+      <style jsx>{defaultStyle}</style>
+
+      <style jsx>
+        {`
+          img {
+            display: ${hovered ? 'none' : 'block'};
+          }
+          .logo-eyes {
+            display: ${hovered ? 'block' : 'none'};
+          }
+        `}
+      </style>
+    </React.Fragment>
+  )
+);
