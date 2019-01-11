5cea4136d9f9a62c7d050face0e8026399d41db9
Howard Chen <chenh0886@gmail.com>
2019-01-10 22:48:49 -0800

    Eliminate unused dependencies

--- pages/work.js
+++ pages/work.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import { compose, withStateHandlers, lifecycle } from 'recompose';
-import viewport from '../ui/spacing/viewport';
-import fontFamily from '../ui/styles/fontFamily';
-import fontSize from '../ui/spacing/fontSize';
-import letterSpacing from '../ui/spacing/letterSpacing';
-import colors from '../ui/styles/colors';
-import spacing from '../ui/spacing/module';
 import Head from '../ui/components/head';
 import Nav from '../ui/components/Nav';
 import Footer from '../ui/components/Footer';
-import TextLink from '../ui/components/TextLink';
-import Paragraph from '../ui/components/Paragraph';
-import Heading from '../ui/components/Heading';
-import Section from '../ui/components/Section';
 import Layout from '../ui/components/Layout';
 import WorkModule from '../ui/components/WorkModule';
 
