19440600f3289368093cf95ed851339bbfbb7c00
Howard Chen <chenh0886@gmail.com>
2019-01-21 18:11:24 -0800

    Fix broken build

--- pwa/exportFiles.prod.js
+++ pwa/exportFiles.prod.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
-fs.createReadStream('./registerServiceWorker.js').pipe(fs.createWriteStream('../out/registerServiceWorker.js'));
-fs.createReadStream('./sw.js').pipe(fs.createWriteStream('../out/sw.js'));
+fs.createReadStream('./pwa/registerServiceWorker.js').pipe(fs.createWriteStream('./out/registerServiceWorker.js'));
+fs.createReadStream('./pwa/sw.js').pipe(fs.createWriteStream('./out/sw.js'));
